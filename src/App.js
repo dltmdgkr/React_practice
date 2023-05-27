fbdbcf65121b392be48901171bb880dcfb93c68f
dltmdgkr <shl907@naver.com>
2023-05-27 19:47:24 +0900

    Movie App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Movie from "./components/Movie";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
+    .then((response) => response.json()).then((json) => {
+      setMovies(json.data.movies);
+      setLoading(false);
+    })
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? (
+        <strong>Loading...</strong>
+        ) : (
+          <div>
+            {movies.map((movie) =>(
+              <Movie
+                key={movie.id}
+                coverImg={movie.medium_cover_image}
+                title={movie.title}
+                summary={movie.summary}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )
+      }
     </div>
   );
 }
