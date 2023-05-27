4a6f48e5db3bfb1560d2d5d1fba44e3ebfeb6609
dltmdgkr <shl907@naver.com>
2023-05-27 19:52:43 +0900

    movie app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,5 @@
-import { useEffect, useState } from "react";
-import Movie from "./components/Movie";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState();
-  useEffect(() => {
-    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
-    .then((response) => response.json()).then((json) => {
-      setMovies(json.data.movies);
-      setLoading(false);
-    })
-  }, [])
-  return (
-    <div>
-      {loading ? (
-        <strong>Loading...</strong>
-        ) : (
-          <div>
-            {movies.map((movie) =>(
-              <Movie
-                key={movie.id}
-                coverImg={movie.medium_cover_image}
-                title={movie.title}
-                summary={movie.summary}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )
-      }
-    </div>
-  );
+  return null;
 }
 
 export default App;
